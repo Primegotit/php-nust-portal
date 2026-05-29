72032908b6dceaed1221fd6a7cb948fddf7b4115
Prime <primevaldigitals@gmail.com>
2026-05-29 03:48:38 +0200

    adding program flow

--- Path-selector.php
+++ Path-selector.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Path selector</title>
     <link rel="stylesheet" href="Admin-Dashboard.css">
     <link rel="stylesheet" href="forms.css">
     <link rel="stylesheet" href="global.css">
@@ -16,7 +16,7 @@
     <div id="admin-dashboard-container">
 
         <nav id="top-heading">
-            <h2>Admin Dashboard 🔍  </h2>
+            <h2>Path selector 🔍  </h2>
         </nav>
 
         <div id="bottom-border"></div>
@@ -24,20 +24,20 @@
         <div id="admin-dashboard-envelope">
 
             <div id="admin-dashboard-content">
-                <h3>Choose your destiny 🧩</h3>
+                <h3>Choose your path </h3>
                 <div id="admin-dashboard-button-container">
-                    <a href="Admin-Form.php" id="destiny-btn">
-                        <p>📌</p>
-                        <h2>New student Registration</h2>
+                    <a href="login-page.php" id="destiny-btn">
+                        <p>👤</p>
+                        <h2>Student</h2>
                     </a>
-                    <a href="Module-manager.php" id="destiny-btn">
-                        <p>📌</p>
-                         <h2>Module manager</h2>
+                    <a href="Admin-dashboard.php" id="destiny-btn">
+                        <p>🛡️</p>
+                         <h2>Administrator</h2>
                     </a>
 
-                    <a href="Register-Form-Admin.php" id="destiny-btn">
-                        <p>📌</p>
-                         <h2>Registration Manager</h2>
+                    <a href="Lecturer-Form.php" id="destiny-btn">
+                        <p>👩‍🏫</p>
+                         <h2>Lecturer</h2>
                     </a>
                 </div>
             </div>
